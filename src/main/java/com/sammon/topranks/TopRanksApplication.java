f779db7d14410fd2466c2896153b6758b96b2622
Higigigihi <qazplmtest1122@gmail.com>
2021-10-04 01:54:54 +0900

    first commit

---
+++ src/main/java/com/sammon/topranks/TopRanksApplication.java
@@ -0,0 +1,13 @@
+package com.sammon.topranks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopRanksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopRanksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sammon/topranks/TopRanksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sammon.topranks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopRanksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
