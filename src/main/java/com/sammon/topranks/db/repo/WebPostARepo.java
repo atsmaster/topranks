2bc0f3d552dee3378a3ee13aeeb09ab725829ac9
atsMaster <qazplmtest1122@gmail.com>
2022-02-01 14:35:21 +0900

    add post repo ctgr condition

--- src/main/java/com/sammon/topranks/db/repo/WebPostARepo.java
+++ src/main/java/com/sammon/topranks/db/repo/WebPostARepo.java
@@ -1,11 +1,19 @@
 package com.sammon.topranks.db.repo;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.sammon.topranks.db.vo.WebPostA;
 
-@RepositoryRestResource(collectionResourceRel = "posts", path = "posts")
-public interface WebPostARepo extends JpaRepository<WebPostA, Integer>{
 
+@RepositoryRestResource(collectionResourceRel = "posts", path = "posts")
+public interface WebPostARepo extends JpaRepository<WebPostA, Integer> {
+
+	@RestResource(path = "ctgrNo", rel = "ctgrNo")
+	List<WebPostA> findByCtgrNo(@Param("ctgrNo") Integer ctgrNo, Pageable p);
 }
